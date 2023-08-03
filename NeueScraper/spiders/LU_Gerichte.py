d2fe157cdf4bd6b69bb4b1584a9bf822fe95cec3
Luis Ackermann <ackermann@lexferenda.ch>
2023-08-03 13:35:39 +0200

    Pass args and kwargs to Spider initialiser

--- NeueScraper/spiders/LU_Gerichte.py
+++ NeueScraper/spiders/LU_Gerichte.py
@@ -21,8 +21,8 @@ class LU_Gerichte(BasisSpider):
 		request=scrapy.Request(url=self.HOST+self.START_URL, callback=self.parse_form, errback=self.errback_httpbin)
 		return request
 	
-	def __init__(self, ab=None, neu=None):
-		super().__init__()
+	def __init__(self, ab=None, neu=None, _job=None):
+		super(LU_Gerichte).__init__(ab=None, neu=None, _job=None)
 		self.ab=ab
 		self.neu=neu
 		self.request_gen = [self.get_request(ab)]
